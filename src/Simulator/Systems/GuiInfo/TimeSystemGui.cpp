5865092bcab41809375f8bbcb84297049bfe4f23
BossOfTheGym <vladimir.deikun1999@gmail.com>
2020-04-28 01:08:34 +0300

    added output of time

--- src/Simulator/Systems/GuiInfo/TimeSystemGui.cpp
+++ src/Simulator/Systems/GuiInfo/TimeSystemGui.cpp
@@ -16,6 +16,11 @@ namespace sim
 		std::uint64_t maxWarp = timeSystem->getMaxWarp();
 		std::uint64_t warp    = timeSystem->getWarp();
 		ImGui::SliderScalar("Time warp", ImGuiDataType_U64, &warp, &minWarp, &maxWarp);
+
+		float realTime = ecs::toSeconds<float>(timeSystem->getRealTime()).count();
+		float simTime  = ecs::toSeconds<float>(timeSystem->getTime()).count();
+		ImGui::Text("Real time: %.2fs", realTime);
+		ImGui::Text("Sim. time: %.2fs", simTime);
 		timeSystem->setWarp(warp);
 	}
 
